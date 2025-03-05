91e301e794d2c0a3a4d13a0975ad7d01b14e7103
Joseph Antwi <joseph.kyereantwi@heidelbergmaterials.com>
2025-03-05 09:43:43 +0000

    Changed how tenant information is fetched

--- backend/src/core/Odatey.FleetManagementSystem.Application/Features/Tenants/Queries/GetTenantQueryHandler.cs
+++ backend/src/core/Odatey.FleetManagementSystem.Application/Features/Tenants/Queries/GetTenantQueryHandler.cs
@@ -1,17 +1,17 @@
 namespace Odatey.FleetManagementSystem.Application.Features.Tenants.Queries;
 
-public record GetTenantQuery(string TenantId) : IQuery<GetTenantQueryDto>;
+public record GetTenantQuery(string UserId) : IQuery<GetTenantQueryDto>;
     
 public class GetTenantQueryHandler(ITenantRepository repository) 
     : IQueryHandler<GetTenantQuery, GetTenantQueryDto>
 {
     public async Task<GetTenantQueryDto> Handle(GetTenantQuery request, CancellationToken cancellationToken)
     {
-        var tenant = await repository.GetTenantAsync(request.TenantId);
+        var tenant = await repository.GetTenantByUserIdAsync(request.UserId);
 
         if (tenant is null)
         {
-            throw new NotFoundException($"Tenant with id {request.TenantId} does not exist");
+            throw new NotFoundException($"Tenant with id {request.UserId} does not exist");
         }
 
         return new GetTenantQueryDto(tenant.Id.Value, tenant.ConnectionString, tenant.CreatedAt);
---
+++ backend/src/presentation/Odatey.FleetManagementSystem.Contracts/Requests/TenantManagement/GetTenantRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Odatey.FleetManagementSystem.Contracts.Requests.TenantManagement;
+
+public record GetTenantRequest(string UserId);
