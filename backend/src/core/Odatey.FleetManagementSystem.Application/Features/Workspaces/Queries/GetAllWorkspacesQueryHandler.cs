dda457dd9d947346c14e5b6aee874fd65096949d
Joseph Antwi <joseph.kyereantwi@heidelbergmaterials.com>
2025-03-17 11:57:49 +0000

    FE completed adding extra workspace

--- backend/src/core/Odatey.FleetManagementSystem.Application/Features/Workspaces/Queries/GetAllWorkspacesQueryHandler.cs
+++ backend/src/core/Odatey.FleetManagementSystem.Application/Features/Workspaces/Queries/GetAllWorkspacesQueryHandler.cs
@@ -11,6 +11,7 @@ public class GetAllWorkspacesQueryHandler(IAsyncRepository<Workspace> context)
             .ListAllAsync();
 
         return workspaces
+            .OrderByDescending(w => w.CreatedAt)
             .Select(s => new WorkspaceResponse(s.Id.Value, s.WorkspaceTitle, s.CreatedAt, s.UpdatedAt, s.CreatedBy, s.UpdatedBy));
     }
 }
